6a4fc45132058c89606e73a04f68337bc9582765
Alekzus <alexandremarcq58@gmail.com>
2022-03-17 14:18:47 +0100

    add json output to group commands

--- group/get.go
+++ group/get.go
@@ -2,6 +2,7 @@ package group
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strings"
 
@@ -37,6 +38,10 @@ func GroupGet(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return err
 	}
+	jsonOutput, err := cmd.Flags().GetBool("json")
+	if err != nil {
+		return err
+	}
 
 	ctx := util.GetContext()
 
@@ -55,34 +60,58 @@ func GroupGet(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return fmt.Errorf("Getting Group: %w", err)
 	}
-	fmt.Printf("Name: %v\n", name)
-	// Print Memberships
-	if len(columns) != 0 {
-		data := pterm.TableData{columns}
 
-		for _, membership := range memberships {
-			entry := make([]string, len(columns))
-			for i := range columns {
-				switch strings.ToLower(columns[i]) {
-				case "userid":
-					entry[i] = membership.UserID
-				case "isgroupmanager":
-					entry[i] = fmt.Sprint(membership.IsGroupManager)
-				case "username":
-					entry[i] = shellescape.StripUnsafe(membership.Username)
-				case "userfirstname":
-					entry[i] = shellescape.StripUnsafe(membership.UserFirstName)
-				case "userlastname":
-					entry[i] = shellescape.StripUnsafe(membership.UserLastName)
-				default:
-					cmd.SilenceUsage = false
-					return fmt.Errorf("Unknown Column: %v", columns[i])
-				}
-			}
-			data = append(data, entry)
+	if jsonOutput {
+		group, err := client.GetGroup(ctx, id)
+		if err != nil {
+			return err
+		}
+		jsonGroup, err := json.MarshalIndent(group, "", "  ")
+		if err != nil {
+			return err
 		}
 
-		pterm.DefaultTable.WithHasHeader().WithData(data).Render()
+		var tempMap map[string]interface{}
+		if err := json.Unmarshal(jsonGroup, &tempMap); err != nil {
+			return err
+		}
+		tempMap["group_users"] = memberships
+		jsonResult, err := json.MarshalIndent(tempMap, "", "  ")
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(string(jsonResult))
+	} else {
+		fmt.Printf("Name: %v\n", name)
+		// Print Memberships
+		if len(columns) != 0 {
+			data := pterm.TableData{columns}
+
+			for _, membership := range memberships {
+				entry := make([]string, len(columns))
+				for i := range columns {
+					switch strings.ToLower(columns[i]) {
+					case "userid":
+						entry[i] = membership.UserID
+					case "isgroupmanager":
+						entry[i] = fmt.Sprint(membership.IsGroupManager)
+					case "username":
+						entry[i] = shellescape.StripUnsafe(membership.Username)
+					case "userfirstname":
+						entry[i] = shellescape.StripUnsafe(membership.UserFirstName)
+					case "userlastname":
+						entry[i] = shellescape.StripUnsafe(membership.UserLastName)
+					default:
+						cmd.SilenceUsage = false
+						return fmt.Errorf("Unknown Column: %v", columns[i])
+					}
+				}
+				data = append(data, entry)
+			}
+
+			pterm.DefaultTable.WithHasHeader().WithData(data).Render()
+		}
 	}
 	return nil
 }
