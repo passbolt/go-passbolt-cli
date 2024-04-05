3df7993ba00927a5ead40a2788d2954a940830e5
Sai Karthik <kskarthik@disroot.org>
2024-04-05 21:54:18 +0530

    Get version info via ldflag during build time.

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"time"
-
 	"github.com/carlmjohnson/versioninfo"
 	"github.com/passbolt/go-passbolt-cli/cmd"
+	"time"
 )
 
+// this variable is one of the goreleaser's default ldflag
+// which considers the latest git tag as version (the v prefix is stripped)
+// More info: https://goreleaser.com/cookbooks/using-main.version/
+var version string = "(devel)"
+
 func main() {
-	cmd.SetVersionInfo(versioninfo.Version, versioninfo.Revision, versioninfo.LastCommit.Format(time.RFC3339))
+	cmd.SetVersionInfo(version, versioninfo.Revision, versioninfo.LastCommit.Format(time.RFC3339))
 	cmd.Execute()
 }
