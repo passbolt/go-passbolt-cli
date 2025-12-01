260921be90fac31ac314cb9ca2b04f419b83f2b1
Georg Spack <georgynet@gmail.com>
2025-12-01 20:11:40 +0100

    simple refactor, split into several methods

--- resource/list.go
+++ resource/list.go
@@ -16,6 +16,8 @@ import (
 	"github.com/pterm/pterm"
 )
 
+var defaultTableColumns = []string{"ID", "FolderParentID", "Name", "Username", "URI"}
+
 // ResourceListCmd Lists a Passbolt Resource
 var ResourceListCmd = &cobra.Command{
 	Use:     "resource",
@@ -30,38 +32,22 @@ func init() {
 	ResourceListCmd.Flags().Bool("own", false, "Resources that are owned by me")
 	ResourceListCmd.Flags().StringP("group", "g", "", "Resources that are shared with group")
 	ResourceListCmd.Flags().StringArrayP("folder", "f", []string{}, "Resources that are in folder")
-	ResourceListCmd.Flags().StringArrayP("column", "c", []string{"ID", "FolderParentID", "Name", "Username", "URI"}, "Columns to return, possible Columns:\nID, FolderParentID, Name, Username, URI, Password, Description, CreatedTimestamp, ModifiedTimestamp")
+	ResourceListCmd.Flags().StringArrayP("column", "c", defaultTableColumns, "Columns to return, possible Columns:\nID, FolderParentID, Name, Username, URI, Password, Description, CreatedTimestamp, ModifiedTimestamp")
+}
+
+type resourceListConfig struct {
+	favorite       bool
+	own            bool
+	group          string
+	folderParents  []string
+	columns        []string
+	columnsChanged bool
+	jsonOutput     bool
+	celFilter      string
 }
 
 func ResourceList(cmd *cobra.Command, args []string) error {
-	favorite, err := cmd.Flags().GetBool("favorite")
-	if err != nil {
-		return err
-	}
-	own, err := cmd.Flags().GetBool("own")
-	if err != nil {
-		return err
-	}
-	group, err := cmd.Flags().GetString("group")
-	if err != nil {
-		return err
-	}
-	folderParents, err := cmd.Flags().GetStringArray("folder")
-	if err != nil {
-		return err
-	}
-	columns, err := cmd.Flags().GetStringArray("column")
-	if err != nil {
-		return err
-	}
-	if len(columns) == 0 {
-		return fmt.Errorf("You need to specify atleast one column to return")
-	}
-	jsonOutput, err := cmd.Flags().GetBool("json")
-	if err != nil {
-		return err
-	}
-	celFilter, err := cmd.Flags().GetString("filter")
+	config, err := parseResourceListFlags(cmd)
 	if err != nil {
 		return err
 	}
@@ -76,112 +62,176 @@ func ResourceList(cmd *cobra.Command, args []string) error {
 	cmd.SilenceUsage = true
 
 	resources, err := client.GetResources(ctx, &api.GetResourcesOptions{
-		FilterIsFavorite:        favorite,
-		FilterIsOwnedByMe:       own,
-		FilterIsSharedWithGroup: group,
-		FilterHasParent:         folderParents,
+		FilterIsFavorite:        config.favorite,
+		FilterIsOwnedByMe:       config.own,
+		FilterIsSharedWithGroup: config.group,
+		FilterHasParent:         config.folderParents,
 	})
 	if err != nil {
-		return fmt.Errorf("Listing Resource: %w", err)
+		return fmt.Errorf("listing resource: %w", err)
 	}
 
-	resources, err = filterResources(&resources, celFilter, ctx, client)
+	resources, err = filterResources(&resources, config.celFilter, ctx, client)
 	if err != nil {
 		return err
 	}
 
-	if jsonOutput {
-		outputResources := make([]ResourceJsonOutput, len(resources))
-		for i := range resources {
-			_, name, username, uri, pass, desc, err := helper.GetResource(ctx, client, resources[i].ID)
-			if err != nil {
-				return fmt.Errorf("Get Resource %w", err)
-			}
+	if config.jsonOutput {
+		return printJsonResources(ctx, client, resources, config.columnsChanged, config.columns)
+	}
 
-			fullResource := ResourceJsonOutput{
-				ID:                &resources[i].ID,
-				FolderParentID:    &resources[i].FolderParentID,
-				Name:              &name,
-				Username:          &username,
-				URI:               &uri,
-				Password:          &pass,
-				Description:       &desc,
-				CreatedTimestamp:  &resources[i].Created.Time,
-				ModifiedTimestamp: &resources[i].Modified.Time,
-			}
-			outputResources[i] = fullResource
+	return printTableResources(ctx, client, resources, config.columns)
+}
+
+func printJsonResources(
+	ctx context.Context,
+	client *api.Client,
+	resources []api.Resource,
+	isColumnsChanged bool,
+	columns []string,
+) error {
+	outputResources := make([]ResourceJsonOutput, len(resources))
+	for i := range resources {
+		_, name, username, uri, pass, desc, err := helper.GetResource(ctx, client, resources[i].ID)
+		if err != nil {
+			return fmt.Errorf("get resource %w", err)
 		}
 
-		if cmd.Flags().Changed("column") && len(columns) > 0 {
-			filteredMap := make([]map[string]interface{}, len(outputResources))
-			for i := range outputResources {
-				filteredMap[i] = make(map[string]interface{})
-				data, _ := json.Marshal(outputResources[i])
-				var resourceMap map[string]interface{}
-				json.Unmarshal(data, &resourceMap)
+		fullResource := ResourceJsonOutput{
+			ID:                &resources[i].ID,
+			FolderParentID:    &resources[i].FolderParentID,
+			Name:              &name,
+			Username:          &username,
+			URI:               &uri,
+			Password:          &pass,
+			Description:       &desc,
+			CreatedTimestamp:  &resources[i].Created.Time,
+			ModifiedTimestamp: &resources[i].Modified.Time,
+		}
+		outputResources[i] = fullResource
+	}
 
-				for _, col := range columns {
-					col = strings.ToLower(col)
+	if isColumnsChanged {
+		filteredMap := make([]map[string]interface{}, len(outputResources))
+		for i := range outputResources {
+			filteredMap[i] = make(map[string]interface{})
+			data, _ := json.Marshal(outputResources[i])
+			var resourceMap map[string]interface{}
+			json.Unmarshal(data, &resourceMap)
 
-					if val, ok := resourceMap[col]; ok {
-						filteredMap[i][col] = val
-					}
+			for _, col := range columns {
+				col = strings.ToLower(col)
+
+				if val, ok := resourceMap[col]; ok {
+					filteredMap[i][col] = val
 				}
 			}
-
-			jsonResources, err := json.MarshalIndent(filteredMap, "", "  ")
-			if err != nil {
-				return err
-			}
-			fmt.Println(string(jsonResources))
-			return nil
 		}
 
-		jsonResources, err := json.MarshalIndent(outputResources, "", "  ")
+		jsonResources, err := json.MarshalIndent(filteredMap, "", "  ")
 		if err != nil {
 			return err
 		}
 		fmt.Println(string(jsonResources))
-	} else {
-		data := pterm.TableData{columns}
+		return nil
+	}
 
-		for _, resource := range resources {
-			// TODO We should decrypt the secret only when required for performance reasonse
-			_, name, username, uri, pass, desc, err := helper.GetResource(ctx, client, resource.ID)
-			if err != nil {
-				return fmt.Errorf("Get Resource %w", err)
-			}
+	jsonResources, err := json.MarshalIndent(outputResources, "", "  ")
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(jsonResources))
+	return nil
+}
 
-			entry := make([]string, len(columns))
-			for i := range columns {
-				switch strings.ToLower(columns[i]) {
-				case "id":
-					entry[i] = resource.ID
-				case "folderparentid":
-					entry[i] = resource.FolderParentID
-				case "name":
-					entry[i] = shellescape.StripUnsafe(name)
-				case "username":
-					entry[i] = shellescape.StripUnsafe(username)
-				case "uri":
-					entry[i] = shellescape.StripUnsafe(uri)
-				case "password":
-					entry[i] = shellescape.StripUnsafe(pass)
-				case "description":
-					entry[i] = shellescape.StripUnsafe(desc)
-				case "createdtimestamp":
-					entry[i] = resource.Created.Format(time.RFC3339)
-				case "modifiedtimestamp":
-					entry[i] = resource.Modified.Format(time.RFC3339)
-				default:
-					cmd.SilenceUsage = false
-					return fmt.Errorf("Unknown Column: %v", columns[i])
-				}
-			}
-			data = append(data, entry)
+func printTableResources(
+	ctx context.Context,
+	client *api.Client,
+	resources []api.Resource,
+	columns []string,
+) error {
+	data := pterm.TableData{columns}
+
+	for _, resource := range resources {
+		// TODO We should decrypt the secret only when required for performance reasons
+		_, name, username, uri, pass, desc, err := helper.GetResource(ctx, client, resource.ID)
+		if err != nil {
+			return fmt.Errorf("get resource %w", err)
 		}
 
-		pterm.DefaultTable.WithHasHeader().WithData(data).Render()
+		entry := make([]string, len(columns))
+		for i := range columns {
+			switch strings.ToLower(columns[i]) {
+			case "id":
+				entry[i] = resource.ID
+			case "folderparentid":
+				entry[i] = resource.FolderParentID
+			case "name":
+				entry[i] = shellescape.StripUnsafe(name)
+			case "username":
+				entry[i] = shellescape.StripUnsafe(username)
+			case "uri":
+				entry[i] = shellescape.StripUnsafe(uri)
+			case "password":
+				entry[i] = shellescape.StripUnsafe(pass)
+			case "description":
+				entry[i] = shellescape.StripUnsafe(desc)
+			case "createdtimestamp":
+				entry[i] = resource.Created.Format(time.RFC3339)
+			case "modifiedtimestamp":
+				entry[i] = resource.Modified.Format(time.RFC3339)
+			default:
+				return fmt.Errorf("unknown column: %v", columns[i])
+			}
+		}
+		data = append(data, entry)
 	}
+
+	pterm.DefaultTable.WithHasHeader().WithData(data).Render()
 	return nil
+}
+
+func parseResourceListFlags(cmd *cobra.Command) (*resourceListConfig, error) {
+	favorite, err := cmd.Flags().GetBool("favorite")
+	if err != nil {
+		return nil, err
+	}
+	own, err := cmd.Flags().GetBool("own")
+	if err != nil {
+		return nil, err
+	}
+	group, err := cmd.Flags().GetString("group")
+	if err != nil {
+		return nil, err
+	}
+	folderParents, err := cmd.Flags().GetStringArray("folder")
+	if err != nil {
+		return nil, err
+	}
+	columns, err := cmd.Flags().GetStringArray("column")
+	if err != nil {
+		return nil, err
+	}
+	if len(columns) == 0 {
+		return nil, fmt.Errorf("you need to specify at least one column to return")
+	}
+	jsonOutput, err := cmd.Flags().GetBool("json")
+	if err != nil {
+		return nil, err
+	}
+	celFilter, err := cmd.Flags().GetString("filter")
+	if err != nil {
+		return nil, err
+	}
+
+	return &resourceListConfig{
+		favorite:       favorite,
+		own:            own,
+		group:          group,
+		folderParents:  folderParents,
+		columns:        columns,
+		columnsChanged: cmd.Flags().Changed("column"),
+		jsonOutput:     jsonOutput,
+		celFilter:      celFilter,
+	}, nil
 }
